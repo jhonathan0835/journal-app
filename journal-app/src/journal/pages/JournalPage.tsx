ffb8168e5b1d728adc951b773990cdafb8392490
jrodriguezra <jrodriguezra@indracompany.com>
2025-04-30 15:13:29 -0500

    Se crea boton flotante

--- journal-app/src/journal/pages/JournalPage.tsx
+++ journal-app/src/journal/pages/JournalPage.tsx
@@ -1,16 +1,31 @@
-import JournalLayout from "../layout/JournalLayout"
-import NoteView from "../views/NoteView"
+import { IconButton } from "@mui/material";
+import JournalLayout from "../layout/JournalLayout";
+import NoteView from "../views/NoteView";
+import NothingSelectedView from "../views/NothingSelectedView";
+import { AddOutlined } from "@mui/icons-material";
 
 const JournalPage = () => {
   return (
     <JournalLayout>
-      
       {/* Views */}
-      {/* <NothingSelectedView /> */}
-        <NoteView />
-      
-    </JournalLayout>
-  )
-}
+      <NothingSelectedView />
+      {/* <NoteView /> */}
 
-export default JournalPage
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ".hover": { backgroundColor: "error.main", opacity: 0.8 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{fontSize:"30"}} />
+      </IconButton>
+    </JournalLayout>
+  );
+};
+
+export default JournalPage;
