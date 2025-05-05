23ab081e58d66f460c9ae6f89320a77204ceda60
jrodriguezra <jrodriguezra@indracompany.com>
2025-05-05 14:01:09 -0500

    Agregando redux a nuestra aplicación y configurando el store.ts

--- journal-app/src/main.tsx
+++ journal-app/src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import JournalApp from './JournalApp.tsx'
 import { BrowserRouter } from 'react-router'
+import { Provider } from 'react-redux'
+import { store } from './store/store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
         <JournalApp />
     </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
